724e7e60d238bd198698f286115129d758625489
sangmi <jude4264@gmail.com>
2022-11-15 10:58:37 +0900

    [UPDATE] teact-button-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  const [count, setcount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h3 data-testid="counter">{count}</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
