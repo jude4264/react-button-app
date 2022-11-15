097c3e7294eca89601bf7af53fda9d20c36d0c38
sangmi <jude4264@gmail.com>
2022-11-15 13:41:17 +0900

    [UPDATE] TDD 연습

--- src/App.js
+++ src/App.js
@@ -4,11 +4,31 @@ import { useState } from 'react';
 function App() {
 
   const [count, setcount] = useState(0)
+  const [disabled, setdisabled] = useState(false)
 
   return (
     <div className="App">
       <header className="App-header">
-       <h3 data-testid="counter">{count}</h3>
+        <h3 data-testid="counter">{count}</h3>
+        <button
+          data-testid="minus-button"
+          onClick={() => setcount((prev) => prev - 1)}
+          disabled={disabled}>
+         
+          -
+        </button>
+        <button
+          data-testid="plus-button"
+          onClick={() => setcount((prev) => prev + 1)}
+          disabled={disabled}>
+          +
+        </button>
+        <button 
+        style={{backgroundColor : "blue"}}
+        data-testid="on/off-button"
+        onClick={()=>setdisabled(prev=>!prev)}>
+          on/off
+        </button>
       </header>
     </div>
   );
